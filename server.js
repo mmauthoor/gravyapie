43bf95908c1f1af85fd475589362527c1a094a08
brendonwang9 <brendonwang30@gmail.com>
2022-01-25 10:34:56 +1100

    database_wrapup and all_stations

--- server.js
+++ server.js
@@ -4,43 +4,25 @@ const app = express();
 const testServo = require('./models/data.js')
 const { Pool } = require('pg');
 const pool = new Pool({
-    database: 'gravyapie'
+    database: 'gravyapie',
+    username: 'root',
+    password: 'password',
 });
-// username: 'harry',
-// password: 'password',
 
 app.use(express.json());
 // app.use(express.static("public"));
 
-// create array for data.js where state is in Victoria
-// change the json file to js, eg data.json > data.js
-let stationsArray = []
-let stations = testServo
-stations.forEach(station => {
-    if(stationsArray.length === 500){
-        return
-    } else {
-        if(station["STATE"] === "Victoria") {
-            stationsArray.push(station)
-        }
-    }
-})
+
+
+app.get("/api/stations/all", (req, res) => {
+    let sql = "select * from servos;"
+    pool.query(sql, params = [], (err, dbres) => {
+        res.send(dbres)
+    });
+});
 
 app.get("/", (req, res) => {
-    // for each station, add in new data to servo table
-    stationsArray.forEach(station => {
-        // create template for sql
-        let sql = `INSERT INTO servos (name, owner, street_address, suburb, state, latitude, longitude) VALUES ($1, $2, $3, $4, $5, $6, $7);`
-        let params = [ station["NAME"], station["OWNER"], station["ADDRESS"], station["SUBURB"], station["STATE"], station["LATITUDE"], station["LONGITUDE"]]
-        pool.query(sql, params, (err, dbres) => {
-            // if an error occurs, show what data is giving error, and what the error is
-            if(err) {
-                console.log(station)
-                return console.error('did not work', err.stack)
-            }
-        });
-    })
-    res.json(stationsArray)
+
 });
 
 app.listen(8080, () => {
